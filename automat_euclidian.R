a9b9c86a7ff355b29f1c597d50b004e25e429cf0
sespesogil <42008709+sespesogil@users.noreply.github.com>
2019-02-11 11:34:08 -0800

    Create automat_euclidian.R

---
+++ automat_euclidian.R
@@ -0,0 +1,28 @@
+
+source("./beads.to.euc.R")
+options(echo=T)
+args <- commandArgs(trailingOnly = TRUE)
+beads <- args[1]
+ROI <- args[2]
+
+euclidian_extraction <- beads.to.euc(beads,ROI)
+rownames(euclidian_extraction) <- euclidian_extraction$bead_ID
+euclidian_distances <-as.matrix(dist(euclidian_extraction[-1], method = "euclidian", diag = TRUE, upper = TRUE))
+col_groups <- substr(colnames(euclidian_distances), 1, 1)
+table(col_groups)
+mat_col <- data.frame(group = col_groups)
+rownames(mat_col) <- colnames(nba)
+mat_colors <- list(group = brewer.pal(3, "Set1"))
+names(mat_colors$group) <- unique(col_groups)
+pheatmap(
+  mat               = euclidian_distances,
+  color             = rainbow(1000),
+  border_color      = NA,
+  show_colnames     = TRUE,
+  show_rownames     = TRUE,
+  annotation_col    = mat_col,
+  annotation_colors = mat_colors,
+  drop_levels       = TRUE,
+  fontsize          = 2,
+  main              = "Euclidian_distances"
+)
