1a55c251604ad38ecc8333ea1a73b3e02859390b
sespesogil <42008709+sespesogil@users.noreply.github.com>
2022-05-15 07:32:51 -0700

    Update beads.to.euc.R

--- beads.to.euc.R
+++ beads.to.euc.R
@@ -3,6 +3,7 @@ beads.to.euc <- function(beads_IDS, ROI) {
 a <- read.table(beads_IDS, sep = "\t", col.names=c("ID","x","y","z"))
 b <- read.table(ROI, sep = "\t", col.names=c("ID"))
 matrix.euclidian = merge(a, b, by=c("ID"))
-final_table<-matrix.euclidian [c('bead_ID','x','y','z')]
+colnames(matrix.euclidian)<-c('bead_ID','x','y','z')
+final_table<-matrix.euclidian[c('bead_ID','x','y','z')]
 return(final_table)
 }
