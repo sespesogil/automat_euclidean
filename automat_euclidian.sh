6d6ef5f1f448cf41de81007d7f240bc93f633a7a
sespesogil <42008709+sespesogil@users.noreply.github.com>
2022-05-15 07:54:21 -0700

    Added bedtools intersect to fully automate the process

--- automat_euclidian.sh
+++ automat_euclidian.sh
@@ -5,9 +5,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 cmm=$1
 
-echo "Extracting bead.ids" 
+echo "Extracting bead.ids"
 grep beadID $cmm > beads
 awk -F' ' '{print $11"\t"$3"\t"$4"\t"$5}' beads | sed -e "s/[beadIDxyz=/>]\+//g" | sed 's/\"//g'  > bead.ids
 rm beads
 
-R --vanilla bead.ids $2 < automat_euclidian.R
+echo "Getting ROI beads"
+awk -F["_:-"] '{print $1"\t"$3"\t"$4}' bead.ids | awk -F" " '{print $1"\t"$2"\t"$3}' > tmp
+paste tmp bead.ids > bead.bedfile
+bedtools intersect -a bead.bedfile -b $2 -wa | awk -F"\t" '{print $4}' | sort | uniq > ROI
+
+Rscript automat_euclidian.R bead.ids ROI
+
