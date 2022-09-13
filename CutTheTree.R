8da868cb02e447d957c09e3517ae17b6a6e5a97b
sespesogil <42008709+sespesogil@users.noreply.github.com>
2022-09-13 13:08:15 -0700

    Update CutTheTree.R

--- CutTheTree.R
+++ CutTheTree.R
@@ -19,7 +19,6 @@ require(ComplexHeatmap)
 require(gridExtra)
 require(dendextend)
 
-
 CutTheTree<-function(EuclideanMatrix1, EuclidianMatrix2 , Quantile=0.95){
 
 # Extract EuM1:
@@ -43,8 +42,8 @@ EuM1= pheatmap::pheatmap(
   main              = "Euclidian_distances"
 )
 
-# HeigthThresold1<-quantile(EuM1$tree_row$height, probs = Quantile)
-HeigthThresold1<-125
+HeigthThresold1<-quantile(EuM1$tree_row$height, probs = Quantile)
+
 
 DendroPlot1<-ggdendrogram(EuM1$tree_row, rotate = TRUE, theme_dendro = FALSE) + geom_hline(aes(yintercept=HeigthThresold1),
                                                                               color="blue", linetype="dashed", size=1) + theme(axis.text=element_text(size=6))
@@ -78,8 +77,8 @@ EuM2= pheatmap::pheatmap(
   main              = "Euclidian_distances"
 )
 
-# HeigthThresold2<-quantile(EuM2$tree_row$height, probs = Quantile)
-HeigthThresold2<-125
+HeigthThresold2<-quantile(EuM2$tree_row$height, probs = Quantile)
+
 
 DendroPlot2<-ggdendrogram(EuM2$tree_row, rotate = TRUE, theme_dendro = FALSE) + geom_hline(aes(yintercept=HeigthThresold2),
                                                                                            color="blue", linetype="dashed", size=1) + theme(axis.text=element_text(size=6))
@@ -92,56 +91,88 @@ HistogramHeigthPlot2<- ggplot(ToHist, aes(x= EuM2.tree_row.height)) +  geom_hist
 
 Clusters2<-data.frame(sort(cutree(EuM2$tree_row, h=HeigthThresold2))) %>% setNames("Clusters_Matrix2")
 
-# plot dendogram plots:
-pdf("DendogramsPlot2.pdf",width=8,height=8)
-DendroPlot<-plot_grid(HistogramHeigthPlot1, HistogramHeigthPlot2, 
-          DendroPlot1, DendroPlot2)
-DendroPlot
-
-dev.off()
-          
-
 SummaryDF<-cbind(Clusters1, Clusters2)
-
-pdf("DomainAssociatedClusters.pdf",width=15,height=8)
 
 pheatmap(t(SummaryDF), fontsize = 7)
 
-dev.off()
+# plot dendogram plots:
+# pdf("DendogramsPlot2.pdf",width=15,height=8)
 
-SummaryClusters<-data.frame(t(data.frame(
-Number_clusters_M1=length(unique(as.numeric(Clusters1$Clusters_Matrix1))),
-Number_clusters_M2=length(unique(as.numeric(Clusters2$Clusters_Matrix2)))))) %>% setNames("Number of clusters")
-SummaryClusters$group<-rownames(SummaryClusters)
+p3 = grid.grabExpr(draw(pheatmap(t(SummaryDF), fontsize = 7)))
+
+p1<-plot_grid(HistogramHeigthPlot1, HistogramHeigthPlot2, 
+              DendroPlot1, DendroPlot2,nrow=2)
 
 
+p3 = grid.grabExpr(draw(pheatmap(t(SummaryDF), fontsize = 7)))
+
+plots <- align_plots(p3, p1, align = 'v', axis = 'l')
+upper_row <- plot_grid(plots[[2]])
+
+CutTheTreePlot<-plot_grid(upper_row, plots[[1]],ncol = 1)
+
+return(CutTheTreePlot)
+
+}
+
+#' UntangeTheThree
+#' Functions to compare number trees
+#' @author sergio.espeso-gil
+#' @param EuclideanMatrix1 Euclidean distances matrix 1 
+#' @param EuclideanMatrix2 Euclidean distances matrix 2
+#' @param Untaggling apply untanggling 
+#' @param Method methodology to untanggle the trees
+#' @Quantile  quantile value to be used 
+#' @example CutTheTree(EuclideanMatrix1, EuclideanMatrix2, Quantile=0.95)
+#' @export 
+
+UntangeTheThree<-function(EuclideanMatrix1, EuclidianMatrix2, Untaggling=TRUE, Method="step2side"){
+  
+  
+  EuM1= pheatmap::pheatmap(
+    mat               = EuclideanMatrix1,
+    color             = rainbow(1000),
+    border_color      = NA,
+    show_colnames     = TRUE,
+    show_rownames     = TRUE,
+    annotation_col    = mat_col,
+    annotation_colors = mat_colors,
+    drop_levels       = TRUE,
+    fontsize          = 2,
+    main              = "Euclidian_distances"
+  )
+  
+  
+  EuM2= pheatmap::pheatmap(
+    mat               = EuclideanMatrix2,
+    color             = rainbow(1000),
+    border_color      = NA,
+    show_colnames     = TRUE,
+    show_rownames     = TRUE,
+    annotation_col    = mat_col,
+    annotation_colors = mat_colors,
+    drop_levels       = TRUE,
+    fontsize          = 2,
+    main              = "Euclidian_distances"
+  )
+  
+  
 D1<-as.dendrogram(EuM1$tree_row)
 D2<-as.dendrogram(EuM2$tree_row)
 dl <- dendlist(D1, D2)
 
+if (Untaggling == FALSE){
+  
+UntangeTheThree<-tanglegram(dl)
+  
+} else {
 
-pdf("DiffDendroGram.pdf",width=15,height=8)
+  Ut <- dl %>% untangle(method = Method) 
+  UntangeTheThree<-Ut %>% plot(main = paste("entanglement =", round(entanglement(Ut), 2))) 
+  
+}
 
-dend_diff(D1, D2)
-
-dev.off()
-
-
-pdf("TangleGramDefault.pdf",width=15,height=8)
-
-dl <- dendlist(D1, D2)
-tanglegram(dl)
-
-dev.off()
-
-
-
-pdf("TangleGramUntaggled.pdf",width=15,height=8)
-
-x <- dl %>% untangle(method = "step2side") 
-x %>% plot(main = paste("entanglement =", round(entanglement(x), 2)))
-
-dev.off()
-
+return(UntangeTheThree)
 
 }
+
